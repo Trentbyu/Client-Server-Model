51db4d7546953abb1eab32261194dd08abc3dfb8
Trentbyu <trentblack@byui.edu>
2022-10-04 11:45:11 -0600

    done for now

--- Client_side.py
+++ Client_side.py
@@ -1,10 +1,10 @@
 import socket
 import time
-HEADER = 64
-PORT = 5051
+HEADER = 32
+PORT = 5005
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = "192.168.0.181"
+SERVER = "192.168.0.214"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,7 +15,7 @@ def send(msg):
     msg_length = len(message)
     send_length = str(msg_length).encode(FORMAT)
     send_length += b' ' * (HEADER - len(send_length))
-    client.send(send_length)
+    client.send(send_length) 
     client.send(message)
     print(client.recv(2048).decode(FORMAT))
 
