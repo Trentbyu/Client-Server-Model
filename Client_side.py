12c8dca8fbbde6ff10cf59293627a15791d51ac2
Linux - Trent <trentblack@byui.edu>
2022-10-04 10:34:48 -0600

    Change

--- Client_side.py
+++ Client_side.py
@@ -4,7 +4,7 @@ HEADER = 64
 PORT = 5051
 FORMAT = 'utf-8'
 DISCONNECT_MESSAGE = "!DISCONNECT"
-SERVER = socket.gethostbyname(socket.gethostname())
+SERVER = "192.168.0.181"
 ADDR = (SERVER, PORT)
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
